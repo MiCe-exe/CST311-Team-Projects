974babb197bc9deed41969d4cfb8e316d5d8e3d1
Michael <indiedevmichael@gmail.com>
2023-09-20 09:10:14 -0700

    Update client.py
    change the logic with the IF statement to have else.

--- PA2/client.py
+++ PA2/client.py
@@ -2,7 +2,8 @@ import socket
 import time
 
 #Server address and port
-server_address = ('localhost', 12000)
+server_address = '10.0.0.1'
+server_port = 12000
 
 #Create a UDP socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,12 +12,11 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 num_pings = 10
 
 #Initialize variables for statistics
-min_rtt = 0.0
+min_rtt = 1000.0
 max_rtt = 0.0
 tot_rtt = 0.0
 avg_rtt = 0.0
 packets_lost = 0
-start_time = 0.0
 
 ALPH = .125
 BETA = .25
@@ -34,32 +34,34 @@ for ping in range(1, num_pings + 1):
     message = f"Ping {ping}: "    
     try:
         #Send the ping request
-        client_socket.sendto(message.encode(), server_address)
+        client_socket.sendto(message.encode(), (server_address, server_port))
         
         #Receive the response
-        response = client_socket.recvfrom(1024)
+        response, _ = client_socket.recvfrom(1024)
             
         #Calculate round-trip time (RTT), convert to ms
+        #rtt = 0.0
         rtt = (time.time() - start_time) * 1000
-        
-        #Setting ping 1's estimated and deviation RTT
-        #Also setting min and max rtt
-        if(num_pings==1):
-            rtt_est, rtt_dev, min_rtt, max_rtt = rtt
-            
+
         #Recalculate min, max, and total rtt
         tot_rtt += rtt
         if(rtt<min_rtt):
             min_rtt = rtt
         if(rtt>max_rtt):
             max_rtt = rtt
-            
-        #Calculate estimated RTT
-        rtt_est = (1-ALPH)*rtt_est+ALPH*rtt
+
+        #Setting ping 1's estimated and deviation RTT
+        #Also setting min and max rtt
+        if(ping==1):
+            rtt_est, rtt_dev, min_rtt, max_rtt = rtt, rtt/2, rtt, rtt #/1.5 = clsoer results
+            #rtt_dev = rtt_est / 2
+        else:
+            #Calculate estimated RTT
+            rtt_est = ((1-ALPH)*rtt_est)+(ALPH*rtt)
         
-        #Calculate deviation RTT
-        rtt_dev = (1-BETA)*rtt_dev+BETA*abs(rtt-rtt_est)
-        
+            #Calculate deviation RTT
+            rtt_dev = ((1-BETA)*rtt_dev)+(BETA*abs(rtt-rtt_est))
+
         print(message + "sample rtt = %.3f"%rtt 
               + " ms, estimated_rtt = %.3f"%rtt_est 
               + " ms, dev_rtt = %.3f"%rtt_dev)
