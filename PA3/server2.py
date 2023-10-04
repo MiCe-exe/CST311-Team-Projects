fc530fbc755faf9c6b3e6092931151d53c3dbdeb
MiCe-exe <indiedevmichael@gmail.com>
2023-10-04 00:35:06 -0700

    Fixed and finished

--- PA3/server2.py
+++ PA3/server2.py
@@ -24,14 +24,16 @@ serverMsg = "X: \"\", Y: \"\""
 counter = 0
 MAX = 2
 clientList = []
+#msgQueue = []
 #lock = threading.Lock()
 
 def debugPrt(msg):
     print("[[|||||| Thread:" + str(msg))
 
 def connection_handler(connection_socket):
-    # Old code
     global clientList
+    global MAX
+    #global msgQueue
 
     query = connection_socket.recv(1024)
     query_decoded = query.decode()
@@ -43,34 +45,36 @@ def connection_handler(connection_socket):
     id = clientList.index(connection_socket)
     
     client = threading.Thread(target=msgThread, args=(query_decoded, id))
-    
+    #msgQueue.append(query_decoded)
+    #print(str(threading.active_count()))
     client.start()
 
-    #time.sleep(1)
+    time.sleep(1)
 
     client.join()
 
-    connection_socket.send(serverMsg.encode())
-    connection_socket.close()
+    # connection_socket.send(serverMsg.encode())
+    # connection_socket.close()
 
-    # while True:
-    #     if t.active_count() >= MAX:
-    #         t.Thread.join()
 
 def msgThread(msg, id):
     global serverMsg
 
+    #lock = threading.Lock()
+
+    #lock.acquire()
     if id == 0:
         serverMsg = serverMsg[:4] + msg + serverMsg[4:]
-        #time.sleep(10)
+        time.sleep(10)
     elif id == 1:
         serverMsg = serverMsg[:(len(serverMsg)-1)] + msg + serverMsg[(len(serverMsg)-1):]
     else:
         serverMsg = serverMsg + ", " + str(id) + ":{}".format(msg)
+    #lock.release()
 
 def main():
     global clientList
-    #global lock
+    global lock
 
     server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 
@@ -96,8 +100,14 @@ def main():
 
         for i in clientList:
             connection_handler(i)
+        
+        #close connections and send msg
+        for i in clientList:
+            i.send(serverMsg.encode())
+            i.close()
 
     finally:
+        print("Server closed")
         server_socket.close()
 
 if __name__ == "__main__":
