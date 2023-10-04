fa28aae368f61a30a2c5ce8e263ca40570fd331c
MiCe-exe <indiedevmichael@gmail.com>
2023-10-04 01:07:35 -0700

    Server and server2 are the same. Added commits

--- PA3/server.py
+++ PA3/server.py
@@ -8,6 +8,9 @@ __credits__ = [
   "Ramo Tucakovic"
 ]
 
+# Mulit threading is needed to stop the program from proccessing the first instructions
+# before the second one is process. This cuases a rat race. Threading adds a layer of 
+# synchronize. Both clients can get both inputs from eacher.
 
 import socket as s
 import time
@@ -21,95 +24,84 @@ log.setLevel(logging.DEBUG)
 
 server_port = 12000
 
+#global variables each client might interact with
 serverMsg = "X: \"\", Y: \"\""
 counter = 0
 MAX = 2
+clientList = []
+#msgQueue = []
 
-def connection_handler(connection_socket, address):
-  # Read data from the new connectio socket
-  #  Note: if no data has been sent this blocks until there is data
-  query = connection_socket.recv(1024)
+# connection handler for each client that is created.
+# from here we can use multi Threading to access the messaging queue
+def connection_handler(connection_socket):
+    global clientList
+    global MAX
 
-  # Decode data from UTF-8 bytestream
-  query_decoded = query.decode()
-  
-  # Log query information
-  log.info("Recieved query test \"" + str(query_decoded) + "\"")
+    query = connection_socket.recv(1024)
+    query_decoded = query.decode()
+    log.info("Recieved query test \"" + str(query_decoded) + "\"")
 
-  # Perform some server operations on data to generate response
-  time.sleep(10)
-  response = query_decoded.upper()   #In this case we dont have to convert to upper
-
-  # Sent response over the network, encoding to UTF-8
-  connection_socket.send(response.encode())
-  
-  # Close client socket
-  connection_socket.close()
-
-def cThread(connection_socket, address, userID):
-  lock = threading.Lock()
-  global serverMsg
-
-  query = connection_socket.recv(1024)
-  query_decoded = query.decode()
-  
-  # Log query information
-  log.info("Recieved query test \"" + str(query_decoded) + "\"")
-
-  with lock:
-    if userID == 1:
-      serverMsg = serverMsg[:4] + query_decoded + serverMsg[4:]
-    elif userID == 2:
-      serverMsg = serverMsg[:(len(serverMsg)-1)] + query_decoded + serverMsg[(len(serverMsg)-1):]
-    else:
-      serverMsg = serverMsg + ", " + str(userID) + ":{}".format(query_decoded)
+    # Get client ID, Client X = 0, client Y = 1
+    id = clientList.index(connection_socket)
     
-  
-  #time.sleep(10)
-  connection_socket.send(serverMsg.encode())
-  
+    client = threading.Thread(target=msgThread, args=(query_decoded, id))
+
+    client.start()
+
+    time.sleep(1)
+
+    client.join()
+
+    # connection_socket.send(serverMsg.encode())
+    # connection_socket.close()
+
+
+def msgThread(msg, id):
+    global serverMsg
+
+    # message from both clients merged.
+    if id == 0:
+        serverMsg = serverMsg[:4] + msg + serverMsg[4:]
+        time.sleep(10)
+    elif id == 1:
+        serverMsg = serverMsg[:(len(serverMsg)-1)] + msg + serverMsg[(len(serverMsg)-1):]
+    else:
+        serverMsg = serverMsg + ", " + str(id) + ":{}".format(msg)
+    #lock.release()
 
 def main():
-  # Create a TCP socket
-  # Notice the use of SOCK_STREAM for TCP packets
-  server_socket = s.socket(s.AF_INET,s.SOCK_STREAM)
-  
-  # Assign IP address and port number to socket, and bind to chosen port
-  server_socket.bind(('',server_port))
-  
-  # Configure how many requests can be queued on the server at once
-  server_socket.listen(MAX)
-  
-  # Alert user we are now online
-  log.info("The server is ready to receive on port " + str(server_port))
-  
-  # Surround with a try-finally to ensure we clean up the socket after we're done
-  try:
-    # Enter forever loop to listen for requests
-    while True:
-      # When a client connects, create a new socket and record their address
-      connection_socket, address = server_socket.accept()
-      log.info("Connected to client at " + str(address))
+    global clientList
+    global lock
 
-      global counter
-      counter += 1
-      
-      clientThreadX = threading.Thread(target=cThread, args=(connection_socket, address, counter))
-      clientThreadY = threading.Thread(target=cThread, args=(connection_socket, address, counter))
+    server_socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 
-      clientThreadY.start()
-      clientThreadX.start()
+    server_socket.bind(('', server_port))
 
-      time.sleep(2)
+    server_socket.listen(MAX)
 
-      clientThreadX.join()
-      clientThreadY.join()
-      
+    log.info("The server is ready to reveive on port " + str(server_port))
 
-  finally:
-    print("++++++++++EXIT+++++++++++++")
-    server_socket.close()
+    try:
+        while True:
+            connection_socket, address = server_socket.accept()
+            log.info("connected to client at " + str(address))
 
+            clientList.append(connection_socket)
+
+            if len(clientList) >= MAX:
+                break
+
+        for i in clientList:
+            connection_handler(i)
+        
+        #close connections and send msg
+        for i in clientList:
+            i.send(serverMsg.encode())
+            i.close()
+
+    finally:
+        print("Server closed")
+        server_socket.close()
 
 if __name__ == "__main__":
   main()
--- PA3/server2.py
+++ PA3/server2.py
@@ -8,6 +8,10 @@ __credits__ = [
   "Ramo Tucakovic"
 ]
 
+# Mulit threading is needed to stop the program from proccessing the first instructions
+# before the second one is process. This cuases a rat race. Threading adds a layer of 
+# synchronize. Both clients can get both inputs from eacher.
+
 import socket as s
 import time
 import threading
@@ -20,33 +24,28 @@ log.setLevel(logging.DEBUG)
 
 server_port = 12000
 
+#global variables each client might interact with
 serverMsg = "X: \"\", Y: \"\""
 counter = 0
 MAX = 2
 clientList = []
 #msgQueue = []
-#lock = threading.Lock()
 
-def debugPrt(msg):
-    print("[[|||||| Thread:" + str(msg))
-
+# connection handler for each client that is created.
+# from here we can use multi Threading to access the messaging queue
 def connection_handler(connection_socket):
     global clientList
     global MAX
-    #global msgQueue
 
     query = connection_socket.recv(1024)
     query_decoded = query.decode()
     log.info("Recieved query test \"" + str(query_decoded) + "\"")
-    #time.sleep(5)
-    # response = query_decoded.upper()
-    # connection_socket.send(response.encode())
-    # connection_socket.close()
+
+    # Get client ID, Client X = 0, client Y = 1
     id = clientList.index(connection_socket)
     
     client = threading.Thread(target=msgThread, args=(query_decoded, id))
-    #msgQueue.append(query_decoded)
-    #print(str(threading.active_count()))
+
     client.start()
 
     time.sleep(1)
@@ -60,9 +59,7 @@ def connection_handler(connection_socket):
 def msgThread(msg, id):
     global serverMsg
 
-    #lock = threading.Lock()
-
-    #lock.acquire()
+    # message from both clients merged.
     if id == 0:
         serverMsg = serverMsg[:4] + msg + serverMsg[4:]
         time.sleep(10)
@@ -89,13 +86,9 @@ def main():
             connection_socket, address = server_socket.accept()
             log.info("connected to client at " + str(address))
 
-            
             clientList.append(connection_socket)
-            # for i in clientList:
-            #     print("\n" + str(i) + "\n")
 
             if len(clientList) >= MAX:
-                print("Two clients added?") #remove
                 break
 
         for i in clientList:
